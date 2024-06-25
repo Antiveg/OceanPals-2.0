583de4485b0ab2e31dfcda37aef4c104d87cedba
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2024-06-25 22:18:14 +0700

    adding 404 error pages and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Profile from "./pages/Profile";
 import Event from "./pages/Event";
 import EventDetail from "./pages/EventDetail";
 import Ranking from "./pages/Rank";
+import NotFound from "./pages/NotFound";
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
