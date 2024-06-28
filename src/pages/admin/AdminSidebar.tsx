51fea6f655eda6bf4beac061c321e12dea8a9f94
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2024-06-28 23:13:15 +0700

    finalize before final

--- src/pages/admin/AdminSidebar.tsx
+++ src/pages/admin/AdminSidebar.tsx
@@ -236,7 +236,7 @@ const AdminSidebar: React.FC = () => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
+                className="h-6 w-6 mr-3"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -248,6 +248,7 @@ const AdminSidebar: React.FC = () => {
                   d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                 />
               </svg>
+              Logout
               {isExpanded && <span className="group-hover:text-gray-700">Logout</span>}
             </button>
           </div>
